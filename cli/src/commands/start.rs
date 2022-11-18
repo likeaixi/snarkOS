d08e13a25485cca5edd944f20cf3453cdf925a31
like <likeaixi@gmail.com>
2022-11-18 17:37:31 +0800

    increase min threads of worker

--- cli/src/commands/start.rs
+++ cli/src/commands/start.rs
@@ -244,7 +244,7 @@ impl Start {
         let (num_tokio_worker_threads, max_tokio_blocking_threads, num_rayon_cores_global) =
             // { ((num_cpus::get() / 2).max(1), num_cpus::get(), (num_cpus::get() / 4 * 3).max(1)) };
             // { (num_cpus::get().min(8), 512, num_cpus::get().saturating_sub(8).max(1)) };
-            { (num_cpus::get()*10, 512, (num_cpus::get()*10).max(1)) };
+            { (num_cpus::get()*4, 512, (num_cpus::get()*4).max(1)) };
 
         // Initialize the parallelization parameters.
         rayon::ThreadPoolBuilder::new()
